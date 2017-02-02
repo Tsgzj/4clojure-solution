68ec857538f44891e42d1213beca504c9618a5c9
Wenxiang Sun <swx@tsgzj.me>
2017-02-01 17:19:06 -0800

    Factorial speed comparison

--- test/clojure_playground/core_test.clj
+++ test/clojure_playground/core_test.clj
@@ -3,5 +3,9 @@
             [clojure-playground.core :refer :all]))
 
 (deftest a-test
-  (testing "FIXME, I fail."
+  (testing "Fixed"
     (is (= 1 1))))
+
+(deftest factorial-test
+  (testing "Factorial with factorial-reduce"
+    (is (= (factorial 20) )2432902008176640000)))
