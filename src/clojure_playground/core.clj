5c1ea0ceb1fbfd1112a82e30e52aa077fcb1f594
Wenxiang Sun <swx@tsgzj.me>
2017-02-01 16:54:14 -0800

    Added factorial

--- src/clojure_playground/core.clj
+++ src/clojure_playground/core.clj
@@ -4,3 +4,15 @@
   "I don't do a whole lot."
   [x]
   (println x "Hello, World!"))
+
+(defn factorial
+  ([x] (factorial 1 x))
+  ([acc x]
+   (if (zero? x)
+     acc
+     (recur (*' x acc) (dec x)))))
+
+(defn factorial-reduce
+  "Seems the reduce version is fater, thanks to lazyness of range(?)"
+  [x]
+  (reduce *' (range 1 (inc x))))
