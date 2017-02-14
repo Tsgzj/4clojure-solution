dffed90fd7a34d8a70627106d7c4a94d878fd5f2
Tsgzj <swx@tsgzj.me>
2017-02-14 10:05:31 -0800

    N33

--- src/clojure_playground/4clojure_03x.clj
+++ src/clojure_playground/4clojure_03x.clj
@@ -5,6 +5,11 @@
   [x]
   (mapcat #(conj '() % %) x))
 
+(defn n33
+  "https://www.4clojure.com/problem/33#prob-title Replica Sequence"
+  [lst n]
+  (mapcat #(repeat n %) lst))
+
 (defn n38
   "https://www.4clojure.com/problem/38 Maximum Value"
   [& arg] (reduce (fn [x y] (if (> x y) x y)) arg))
