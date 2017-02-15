33d2b08d325806781518d11154dc76a6b714b73a
Tsgzj <swx@tsgzj.me>
2017-02-15 15:34:08 -0800

    Forgot to commit source code

--- src/clojure_playground/4clojure_03x.clj
+++ src/clojure_playground/4clojure_03x.clj
@@ -10,6 +10,15 @@
   [lst n]
   (mapcat #(repeat n %) lst))
 
+(defn n34
+  "https://www.4clojure.com/problem/34#prob-title Range"
+  [start end]
+  (letfn [(range-helper [start end lst]
+            (if (= start end)
+              lst
+              (range-helper (inc start) end (conj lst start))))]
+    (range-helper start end [])))
+
 (defn n38
   "https://www.4clojure.com/problem/38 Maximum Value"
   [& arg] (reduce (fn [x y] (if (> x y) x y)) arg))
