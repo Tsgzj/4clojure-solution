4d3878f7c0e94226b5deaf60678e37e18548d978
Tsgzj <wxsun1991@gmail.com>
2017-02-16 22:11:16 -0800

    commit

--- src/clojure_playground/4clojure_03x.clj
+++ src/clojure_playground/4clojure_03x.clj
@@ -1,5 +1,7 @@
 (ns clojure-playground.4clojure_03x)
 
+;; for problem 030~039
+
 (defn n38
   "https://www.4clojure.com/problem/38 Maximum Value"
   [& arg] (reduce (fn [x y] (if (> x y) x y)) arg))
