cd495cd4e68574cf795a31464856f2c1d1e30ea9
Tsgzj <swx@tsgzj.me>
2017-04-25 13:40:34 -0700

    Fixed

--- src/fclj/4clojure_08x.clj
+++ src/fclj/4clojure_08x.clj
@@ -11,6 +11,14 @@
   [a b]
   (set (filter a b)))
 
+(defn n81-set-theory
+  "Set Intersection
+    (A∪B∖A)∖B
+    Does not apply here"
+  [a b]
+  (let [u (union a b)]
+    (difference u (difference u a) (difference u b))))
+
 (defn n83
   "Half truth"
   [& args]
