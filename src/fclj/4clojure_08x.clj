b4450c40dc9a9976e58c88e6a70838a6c1b626bc
Tsgzj <swx@tsgzj.me>
2017-04-25 11:24:29 -0700

    modified:   .gitignore

--- src/fclj/4clojure_08x.clj
+++ src/fclj/4clojure_08x.clj
@@ -7,14 +7,6 @@
   (= n
      (apply + (filter #(= 0 (mod n %)) (range 1 n)))))
 
-(defn n81-set-theory
-  "Set Intersection
-  (A∪B∖A)∖B
-  Does not apply here"
-  [a b]
-  (let [u (union a b)]
-    (difference u (difference u a) (difference u b))))
-
 (defn n81
   [a b]
   (set (filter a b)))
