1e0b14ee71715c2726c9f47a24a9a9ff77d8d241
Tsgzj <swx@tsgzj.me>
2017-05-23 18:17:04 -0700

    n148

--- src/fclj/4clojure_14x.clj
+++ src/fclj/4clojure_14x.clj
@@ -9,3 +9,13 @@
   "Pascal's Trapezoid"
   [p]
   (iterate #(vec (map +' (cons 0 %) (conj % 0))) p))
+
+(defn n148
+  [j k l]
+  (letfn [(sum [a n]
+            (/ (* n (+ a (* n a))) 2))
+          (tit [x y]
+            (sum x (bigint (quot (dec y) x))))]
+    (- (+ (tit k j)
+          (tit l j))
+       (tit (* k l) j))))
