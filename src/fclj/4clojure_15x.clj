f13bd712fbda41d45a0ba8cb92948aeae236baea
Tsgzj <swx@tsgzj.me>
2017-07-14 17:12:07 -0700

    n150 in progress

--- src/fclj/4clojure_15x.clj
+++ src/fclj/4clojure_15x.clj
@@ -1,4 +1,18 @@
-(ns fclj.4clojure-15x)
+ (ns fclj.4clojure-15x)
+
+(defn n150
+  "Palindromic Numbers"
+  [n]
+  (letfn [(decode [x]
+            (if (> 10 x) [x] (conj (decode (quot x 10)) (rem x 10))))
+          (encode [x]
+            (reduce #(+ (* 10 %) %2) x))
+          (next-p [n]
+            (let [N (decode n)
+                  L (count N)
+                  H (quot L 2)]
+                ))]
+    (next-p n)))
 
 (defn n153
   "Pairwise Disjoint Set"
