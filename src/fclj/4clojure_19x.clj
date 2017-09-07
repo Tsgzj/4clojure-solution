2c4a5b8475a785457945b23a8bf697f4087f220f
Tsgzj <swx@tsgzj.me>
2017-09-06 17:48:42 -0700

    n195
    	modified:   src/fclj/4clojure_19x.clj

--- src/fclj/4clojure_19x.clj
+++ src/fclj/4clojure_19x.clj
@@ -2,5 +2,12 @@
 
 (defn n195
   "Parentheses...Again"
-  [n]
-  )
+  ([n] (if (zero? n) #{""} (set (n195 n 0))))
+  ([n open]
+   (if (zero? n)
+     (vector (apply str (repeat open ")")))
+     (if (zero? open)
+       (map #(str "(" %) (n195 (dec n) 1))
+       (concat
+        (map #(str ")" %) (n195 n (dec open)))
+        (map #(str "(" %) (n195 (dec n) (inc open))))))))
